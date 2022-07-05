b567346eb1f56738e4cbf364c3a0b2019aaa5af5
sauedum <1550994+sauedum@users.noreply.github.com>
2022-07-05 18:56:13 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Object for wow classes, state and role
+const wowChar = {
+    class: ['warrior', 'warlock', 'priest', 'death-knight', 'monk', 'rogue', 'hunter', 'mage', 'druid', 'demon-hunter', 'shaman', 'paladin'],
+    state: ['meta', 'good', 'decent', 'bench'],
+    role: ['tank', 'dps', 'healer']
+}
